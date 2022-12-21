f17fa28c94bdf2e695600d7752f12878b750ad7f
Mylo Cyrus <irahiggins740@gmail.com>
2022-12-21 08:09:50 -0800

    Add files via upload

---
+++ LG_CHristmas_deploy.cpp
@@ -0,0 +1,20 @@
+{makerfile
+usps}
+~/stamps send mail LG nuevo for christmas aka this }
+		  [@mylocyrus]                                         ~~  monsanto x 3m geo/bio engeneering /more
+}} user github.com = for [mileycyrus] xhtml </09788 (MERRY CHRISTMAS) /> !
+{makerfile
+usps}
+~/stamps send mail LG nuevo for christmas aka this }
+          [@mylocyrus]                                         ~~  monsanto x 3m geo/bio engeneering /more
+}} user github.com = for [mileycyrus] xhtml </09788 (MERRY CHRISTMAS) /> !
+
+{makerfile
+usps}
+~/stamps send mail lg nuevo for christmas aka this }
+          [@mylocyrus]                                         ~~  monsanto x 3m geo/bio engeneering /more
+}} user github.com = for [mileycyrus] xhtml </09788 (merry christmas) /> !
+/who the fuck is dr dre ? "play that ish" delete
+burn dj kahlid/delete
+burn dj kahlid/
+
